db9a2e1d6654b5c371d888fb6533ceb2e37e0a50
tms1989 <stigler.thomas@gmail.com>
2022-03-02 01:16:02 -0500

    with RPC controller scripts & increment based space ID

---
+++ Assets/Scripts/RPC_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class RPC_Controller : MonoBehaviour
+{   
+   private PhotonView PV;
+   public int currentSpaceNumberRPC;
+   public int redDeckNotYetDealt;
+   public int blackDeckNotYetDealt;
+   public int currentSpaceNumber;
+
+    /*void Start(){
+        PV= GetComponent<PhotonView>();
+    }
+
+    void Update(){
+        txtNumber.text="Number to Increase: "+globalNumber;
+    }
+           */
+
+    [PunRPC]
+    void RPC_UpdateCurrentSpaceNumber(){
+        currentSpaceNumberRPC++;
+    }
+
+    [PunRPC]
+    void RPC_ResetCurrentSpaceNumber()
+    {
+        currentSpaceNumberRPC = 0;
+    }
+
+    [PunRPC]
+    void RPC_UpdateRedDeckNotYetDealt()
+    {
+        redDeckNotYetDealt--;
+    }
+
+    [PunRPC]
+    void RPC_UpdateBlackDeckNotYetDealt()
+    {
+        blackDeckNotYetDealt--;
+    }
+}
---
+++ Assets/Scripts/RPC_SpaceController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class RPC_SpaceController : MonoBehaviour
+{   
+   private PhotonView PV;
+   public int spaceRankRPC;
+
+    /*void Start(){
+        PV= GetComponent<PhotonView>();
+    }
+
+    void Update(){
+        txtNumber.text="Number to Increase: "+globalNumber;
+    }
+           */
+
+    [PunRPC]
+    void RPC_UpdateSpaceRank(){
+        spaceRankRPC++;
+    }
+
+    [PunRPC]
+    void RPC_ResetSpaceRank()
+    {
+        spaceRankRPC = 0;
+    }
+
+}
