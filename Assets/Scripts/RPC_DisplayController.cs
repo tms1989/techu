7e4725aa837601e355ea38e6c4986637ac3b750c
tms1989 <stigler.thomas@gmail.com>
2022-03-08 00:29:04 -0500

    includes loading text, other changes

---
+++ Assets/Scripts/RPC_DisplayController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class RPC_Controller : MonoBehaviour
+{   
+   private PhotonView PV;
+   
+    /*void Start(){
+        PV= GetComponent<PhotonView>();
+    }
+
+    void Update(){
+        txtNumber.text="Number to Increase: "+globalNumber;
+    }
+
+    [PunRPC]
+    void RPC_SendString(){
+        currentSpaceNumberRPC++;
+    }
+
+      */
+}
